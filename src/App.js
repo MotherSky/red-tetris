2a36d42c91c76b353224ddc15c6ace64f228bf0e
MotherSky <ayoubtaleb.edu@gmail.com>
2022-10-23 21:30:10 +0100

    popup

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Controls from "./components/Controls";
 import GridBoard from "./components/GridBoard";
 import NextGrid from "./components/NextBlock";
+import Popup from "./components/Popup";
 import ScoreBoard from "./components/ScoreBoard";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <NextGrid />
       <ScoreBoard />
       <Controls />
+      <Popup />
     </div>
   );
 }
---
+++ src/components/Popup.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../App.css";
+
+export default function Popup(props) {
+  return (
+    <div className="message-popup">
+      <h1>Message Title</h1>
+      <p>Message Info</p>
+    </div>
+  );
+}
