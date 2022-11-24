2e10da3ab1a1916f3e238149feaf024926396f1d
MotherSky <ayoubtaleb.edu@gmail.com>
2022-11-24 14:09:35 +0100

    center aligned text in error page

--- src/components/Pages/UrlError.js
+++ src/components/Pages/UrlError.js
@@ -3,12 +3,14 @@ import React from "react";
 function UrlError() {
   return (
     <>
-      <h1 className="text-2xl ">Error in Url</h1>
-      <h3>
-        {
-          "Please use the following format: http://<server_name_or_ip>:<port>/#<room>[<player_name>]"
-        }
-      </h3>
+      <div className="font-pixel uppercase flex flex-col justify-center h-screen text-center">
+        <h1 className="text-5xl text-rose-600 mb-8">Error in Url</h1>
+        <p className="text-2xl">
+          {
+            "Please use the following format: http://<server_name_or_ip>:<port>/#<room>[<player_name>]"
+          }
+        </p>
+      </div>
     </>
   );
 }
