b9dd9b15b4532a7421fd61ef3ca3d473c93492c7
MotherSky <ayoubtaleb.edu@gmail.com>
2023-01-07 07:29:13 +0100

    init testing

---
+++ src/__tests__/MainRoute.test.js
@@ -0,0 +1,8 @@
+import { render } from "@testing-library/react";
+import MainRoute from "../Router/MainRoute";
+
+describe(MainRoute, () => {
+  it("should ", () => {
+    const { get } = render(<MainRoute />);
+  });
+});
