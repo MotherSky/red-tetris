d71e1b83bd24e46039edcfcf7a176c75d8884ca7
Ayoub <ayoubtaleb.edu@gmail.com>
2023-01-09 06:52:52 +0100

    finished testing for popup and scoreboard and edited scoreboard for less branching

--- src/__tests__/Popup.test.js
+++ src/__tests__/Popup.test.js
@@ -1,1 +1,20 @@
-it("pass", () => {});
+import { fireEvent, render, screen } from "@testing-library/react";
+import { Provider, useDispatch } from "react-redux";
+import Popup from "../components/MainGame/Popup";
+import { pause } from "../Slice/GameSlice";
+import store from "../store";
+import ScoreBoard from "../components/MainGame/ScoreBoard";
+
+describe("Popup", () => {
+  it("should render without crashing when the game is paused or over", () => {
+    render(
+      <Provider store={store}>
+        <ScoreBoard />
+        <Popup />
+      </Provider>
+    );
+    fireEvent.click(screen.getByText("PAUSE"));
+    expect(screen.getByText(/the game is paused/i)).toBeInTheDocument();
+  });
+  // gameOver can't be tested because there is nothing that sets it in client
+});
--- src/__tests__/ScoreBoard.test.js
+++ src/__tests__/ScoreBoard.test.js
@@ -1,1 +1,19 @@
-it("pass", () => {});
+import { fireEvent, render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import ScoreBoard from "../components/MainGame/ScoreBoard";
+import store from "../store";
+
+describe("Scoreboard", () => {
+  it("should render without crashing", () => {
+    render(
+      <Provider store={store}>
+        <ScoreBoard />
+      </Provider>
+    );
+    fireEvent.click(screen.getByText("PAUSE"));
+    fireEvent.click(screen.getByText("RESUME"));
+    fireEvent.click(screen.getByText("RESTART"));
+  });
+});
+
+//ScoreBoard.js|   72.72 | 66.66 | 75 | 70 | 19,24-33
