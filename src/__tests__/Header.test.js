2dc08435f13076023f3e9e6dde37a59a641a0b9d
Ayoub <ayoubtaleb.edu@gmail.com>
2023-01-09 17:38:16 +0100

    changed name for slices and finished headerSlice tests

--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -1,5 +1,38 @@
-import Header from "../Slice/HeaderSlice";
+import { fireEvent, render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import store from "../store";
+import Header from "../components/MainGame/header";
+import { initHeaderState } from "../Slice/HeaderSlice";
 
-describe("HeaderSlice", () => {});
+describe("Header", () => {
+  it("should render without crashing", () => {
+    render(
+      <Provider store={store}>
+        <Header />
+      </Provider>
+    );
+  });
 
-//  HeaderSlice.js         |   66.66 |      100 |      50 |   66.66 | 18-19
+  it("should start the game correctly if it's the owner", () => {
+    // mock the header state so to test if we can start the game
+    const header = {
+      inRoom: "room",
+      gameMaster: true,
+      uuid: "6b6b0879-2b0b-47fb-805d-9da240b43845",
+    };
+    store.dispatch(initHeaderState(header));
+    // mock the startGame function
+    let gameStarted = false;
+    function startGame() {
+      gameStarted = true;
+    }
+
+    render(
+      <Provider store={store}>
+        <Header startGame={startGame} />
+      </Provider>
+    );
+    fireEvent.click(screen.getByText("Game Start"));
+    expect(gameStarted).toBeTruthy();
+  });
+});
